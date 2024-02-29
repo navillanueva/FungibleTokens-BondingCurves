64a94a02bd9d873a621d09df88aec245ce3103c1
Nicolas <nicolasarnedo@sdx.com>
2024-02-29 14:03:46 +0100

    fix: removing foudnry boilerplate code

--- src/contract2.sol
+++ src/contract2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GodModeToken is ERC20, ReentrancyGuard, Ownable {
